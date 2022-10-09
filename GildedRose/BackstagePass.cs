74eacd8e757ad052b50711ea7b98d27351cc9f75
Viggo Starcke <vist@itu.dk>
2022-10-09 02:22:03 +0200

    suggestion for BackstagePass

--- GildedRose/BackstagePass.cs
+++ GildedRose/BackstagePass.cs
@@ -2,6 +2,26 @@ namespace GildedRose;
 public class BackstagePass : Item{
 
     public override void UpdateItem(){
+        //My suggestion for avoiding a double-nested condition statement
+        // if (Quality > 50) 
+        // {
+        //     return;
+        // }
+
+        // if (SellIn <= 10 && SellIn > 5)
+        // {
+        //     Quality += 2;
+        // }
+        // if (SellIn <= 5 && SellIn > 0)
+        // {
+        //     Quality += 3;
+        // }
+        // if (SellIn <= 0)
+        // {
+        //     Quality = 0;
+        // }
+
+
         if(Quality<50){
             if(SellIn > 10){
                 Quality++;
